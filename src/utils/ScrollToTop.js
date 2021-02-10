49ed3f22c8b4f507ddb254f8421a3ccf0a738caf
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2021-02-10 07:58:21 +0000

    Added scrolltotop

--- src/utils/ScrollToTop.js
+++ src/utils/ScrollToTop.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 function ScrollToTop() {
+  // function to scroll user back to top on route change
   const { pathname } = useLocation();
 
   useEffect(() => {
